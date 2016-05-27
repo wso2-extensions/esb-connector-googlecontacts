ee7f77ad1814fbe6f9981f1b66cd20544975b2b8
kalyani <ykalyani17@gmail.com>
2016-05-27 16:46:28 +0530

    version change after the release

--- src/test/java/org/wso2/carbon/connector/integration/test/googlecontacts/GoogleContactsConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/googlecontacts/GoogleContactsConnectorIntegrationTest.java
@@ -43,7 +43,7 @@ public class GoogleContactsConnectorIntegrationTest extends ConnectorIntegration
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("googlecontacts-connector-1.0.1");
+        init("googlecontacts-connector-1.0.2-SNAPSHOT");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
